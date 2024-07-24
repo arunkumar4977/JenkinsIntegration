1c69622c4694491ddebdfe0d96397796f27413dd
arunkumar4977 <nakr2107@gmail.com>
2024-07-24 17:34:56 +0530

    Files modified

--- src/test/java/TestRunners/TestRunner.java
+++ src/test/java/TestRunners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(tags="",
-features="\\src\\test\\resources\\features\\login.feature",
+features="src/test/resources/features/login.feature",
 glue={"StepDefinitions","AppHooks"})
 public class TestRunner {
 	
