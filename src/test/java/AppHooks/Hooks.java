5fe940bf30b3cf39c96e8610f19dbcf67bca8217
arunkumar4977 <121481505+arunkumar4977@users.noreply.github.com>
2024-07-25 10:54:10 +0530

    Update Hooks.java

--- src/test/java/AppHooks/Hooks.java
+++ src/test/java/AppHooks/Hooks.java
@@ -32,7 +32,7 @@ public void launch_browser(Scenario scenario) {
 	String browser = prop.getProperty("browser");
 	if(browser.equals("chrome")) {
 		 WebDriver driver = null;
-		   System.setProperty("webdriver.chrome.driver", "C:/Users/NEO/Desktop/Selenimchrome/chromedriver.exe");
+		   System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/linuxdriver/chromedriver");
 		   
 		   ChromeOptions cp = new ChromeOptions();
 		   cp.addArguments("--remote-allow-origins=*");
